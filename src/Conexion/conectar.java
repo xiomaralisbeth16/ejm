b5c15959dd1338f75e9dbedef96263cfb7e6f785
xiomaralisbeth16 <xiomara_lis_16@hotmail.es>
2019-09-27 18:30:53 -0500

    test

---
+++ src/Conexion/conectar.java
@@ -0,0 +1,33 @@
+
+package Conexion;
+
+import java.sql.*;
+import javax.swing.*;
+
+/**
+ *
+ * @author Xiomara
+ */
+public class conectar {
+Connection conect = null;
+   public Connection conexion()
+    {
+      try {
+             
+           //Cargamos el Driver MySQL
+           Class.forName("com.mysql.jdbc.Driver");
+           conect = DriverManager.getConnection("jdbc:mysql://localhost/conexionjava","root","");
+           JOptionPane.showMessageDialog(null, "Usted esta conectado");
+           //Cargamos el Driver Access
+           //Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+           //Conectar en red base 
+           //String strConect = "jdbc:odbc:Driver=Microsoft Access Driver (*.mdb);DBQ=//servidor/bd_cw/cw.mdb";
+           //Conectar Localmente
+           //String strConect = "jdbc:odbc:Driver=Microsoft Access Driver (*.mdb);DBQ=D:/cwnetbeans/cw.mdb";
+          //conect = DriverManager.getConnection(strConect,"",""); 
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,"Error "+e);
+        }
+        return conect;
+     
+}}
